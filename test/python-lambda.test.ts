aa1e79addecf7314530b54717d7e7e36e52df260
Sachin Khamitkar <sachin.bmp@gmail.com>
2023-06-22 15:34:18 +0530

    Initial commit

---
+++ test/python-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PythonLambda from '../lib/python-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/python-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PythonLambda.PythonLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
